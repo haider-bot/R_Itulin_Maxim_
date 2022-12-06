f84c0d3ab30e3f2e7de17e37913fbad5e23d4ad9
Kapellan <58063933+haider-bot@users.noreply.github.com>
2022-12-06 16:26:06 +0300

    CMake1

---
+++ CMakeProject1.cpp
@@ -0,0 +1,9 @@
+﻿// CMakeProject1.cpp: определяет точку входа для приложения.
+//
+
+#include <iostream>
+int main(int argc, char** argv)
+{
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
