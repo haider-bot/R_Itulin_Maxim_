ac2c3e7ecd52528aa68f336061bc3f4b0e7de5ed
Kapellan <58063933+haider-bot@users.noreply.github.com>
2023-09-22 16:12:58 +0300

    Delete CMakeProject1.cpp

--- CMakeProject1.cpp
+++
@@ -1,9 +0,0 @@
-﻿// CMakeProject1.cpp: определяет точку входа для приложения.
-//
-
-#include <iostream>
-int main(int argc, char** argv)
-{
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
-}
